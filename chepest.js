358cd77c5df8c5193bdd4bb5ca919c325966e352
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-02-04 20:59:54 +0600

    if

---
+++ chepest.js
@@ -0,0 +1,16 @@
+const phones=[
+     {name:'samsung s5',price:45000,camera:10,storage:32},
+     {name:'walton m32',price:15000,camera:8,storage:32},
+     {name:'shaomi m3',price:12000,camera:8,storage:32},
+     {name:'oppo a2',price:17000,camera:8,storage:32},
+     {name:'nokia n95',price:8000,camera:8,storage:4},
+     {name:'htc h5',price:25000,camera:10,storage:32}
+];
+let chepest=phones[0];
+for(const phone of phones){
+    //compare price only
+    if(phone.price<chepest.price){
+        chepest=phone;
+    }
+}
+console.log(chepest);
